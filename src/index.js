98f1431b1efffa6d847a9ff75653784bd87ac708
shriyabi <shriyarbiddala@gmail.com>
2026-03-28 23:33:14 -0600

    new

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sb-portfolio-2.0">
       <Routes>
         <Route path="/" element={<App />} />  
         <Route path="/resume" element={<Resume />} />
